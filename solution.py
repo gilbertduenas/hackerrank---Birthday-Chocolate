8d3aadc6d81621ceb7e0e9e5ddb716d895a00485
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-27 21:53:44 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,28 @@
+def birthday(s, d, m, n):
+    i = j = 0
+    total = 0
 
+    if m > n:
+        return 0
+
+    while j < m:
+        total += s[j]
+        j += 1
+
+    if total == d:
+      count = 1 
+
+    else:
+      count = 0
+
+    while j < n:
+        total -= s[i]
+        total += s[j]
+
+        if total == d:
+            count += 1
+
+        i += 1
+        j += 1
+        
+    return count
